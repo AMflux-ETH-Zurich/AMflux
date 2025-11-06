45e2089baa51b12ed4ea3ad34fea0cd5b1bef025
Wendecomprende <contactwendelin@gmail.com>
2025-11-06 12:23:49 +0100

    Initial structure for main.py, network function
    network function connects to network and creates a node

--- src/amflux/app/main.py/main.py
+++ src/amflux/app/main.py/main.py
@@ -1,1 +1,29 @@
-print("Hello world")
+import canopen
+
+
+def network_setup():
+    # Create a CANopen network
+    network = canopen.Network()
+
+    # Connect to a CAN interface (e.g., 'can0' for Linux SocketCAN)
+    network.connect(channel='can0', bustype='socketcan')
+
+    # Add a node to the network with a specific EDS file
+    node = network.add_node(1, 'path/to/your/eds/file.eds') 
+
+    # disconnect from the network
+    network.disconnect()
+
+
+
+
+
+def main():
+    network_setup()
+
+    #blablabal
+    
+
+if __name__ == "__main__":
+
+    main()
