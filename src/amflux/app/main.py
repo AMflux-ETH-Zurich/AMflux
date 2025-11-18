c4085b935765b2d5eddf61511f1efe9460e98a3c
Wendecomprende <contactwendelin@gmail.com>
2025-11-18 17:26:20 +0100

    edited motor_init_setup() function, added ssi_abs_encoder_init_setup() function, added electrical_system_init_setup()

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -32,24 +32,68 @@ def network_setup(node_id: int, node_eds: str, node_channel: str):
     return net, node
 
 
-def motor_init_setup(node, nominal_current: int, current_lim: int, pole_pairs: int, therm_const: int, tor_const: int, ):
+def motor_init_setup(node, motor_type: int=None, nominal_current: int=None, current_lim: int=None, 
+                        pole_pairs: int=None, therm_const: int=None, tor_const: int=None):
     #motor type (Sinusoidal PM BL motor = 10 or in hex 0x0A)
-    node.sdo[0x6402][0].raw = 0x000A
+    node.sdo[0x6402][0x0].raw = motor_type if motor_type is not None else 0x000A
     #nominal current flowing through motor windings in mA
-    node.sdo[0x3001][1].raw = nominal_current
+    node.sdo[0x3001][0x1].raw = nominal_current if nominal_current is not None else 15000
     #output current limit in mA
-    node.sdo[0x3001][2].raw = current_lim
+    node.sdo[0x3001][0x2].raw = current_lim if current_lim is not None else 30000
     #number of pole pairs
-    node.sdo[0x3001][3].raw = pole_pairs
+    node.sdo[0x3001][0x3].raw = pole_pairs if pole_pairs is not None else 1
     #thermal time constant of windings
-    node.sdo[0x3001][4].raw = therm_const
+    node.sdo[0x3001][0x4].raw = therm_const if therm_const is not None else 40
     #torque constant of motor
-    node.sdo[0x3001][5].raw = tor_const
+    node.sdo[0x3001][0x5].raw = tor_const if tor_const is not None else 0
 
-def encoder_init_setup(node, ):
-    print("blabla")
 
-    
+def ssi_abs_encoder_init_setup(node, data_rate: int=None, data_bits: int=None, encoding_type: int=None,
+                                timeout_time: int=None, sbits_trailing_data: int = None, refresh_frequency: int=None,
+                                    power_up_time: int=None, position_raw_value: int=None, commutation_offset_value: int=None,
+                                        position_bits: int=None, sbits_leading_data: int=None, position_raw_value_complete: int=None, 
+                                            communication_additional_delay: int=None):
+    #6.2.58
+    #SSI data rate
+    node.sdo[0x3012][0x1].raw = data_rate if data_rate is not None else 2000 #REFER TO RENISHAW ENCODER SHEET
+    #SSI number of data bits
+    node.sdo[0x3012][0x2].raw = data_bits if data_bits is not None else 0x00000C00 #REFER TO RENISHAW ENCODER SHEET
+    #SSI encoding type
+    node.sdo[0x3012][0x3].raw = encoding_type if encoding_type is not None else 0x001 #REFER TO RENISHAW ENCODER SHEET
+    #SSI timeout time
+    node.sdo[0x3012][0x5].raw = timeout_time if timeout_time is not None else 30 #REFER TO RENISHAW ENCODER SHEET
+    #Special bits trailing data
+    node.sdo[0x3012][0x6].raw = sbits_trailing_data if sbits_trailing_data is not None else 0 #REFER TO RENISHAW ENCODER SHEET
+    #SSI refresh frequency
+    node.sdo[0x3012][0x7].raw = refresh_frequency #REFER TO RENISHAW ENCODER SHEET
+    #SSI Power up time
+    node.sdo[0x3012][0x8].raw = power_up_time if power_up_time is not None else 200 #REFER TO RENISHAW ENCODER SHEET
+    #SSI postition raw value, lower 32 bits
+    node.sdo[0x3012][0x9].raw = position_raw_value
+    #SSI commutaiton offset value
+    node.sdo[0x3012][0xA].raw = commutation_offset_value if commutation_offset_value is not None else 0
+    #SSI position bits
+    node.sdo[0x3012][0xB].raw = position_bits if position_bits is not None else  0x0000000C #REFER TO RENISHAW ENCODER SHEET, MUST BE REDUCED IF >32
+    #SSI special bits leading data
+    node.sdo[0x3012][0xC].raw = sbits_leading_data if sbits_leading_data is not None else 0 #REFER TO RENISHAW ENCODER SHEET
+    #SSI position raw value complemete
+    node.sdo[0x3012][0xD].raw = position_raw_value_complete
+    #SSI communication additional delay
+    node.sdo[0x3012][0xE].raw = communication_additional_delay if communication_additional_delay is not None else -1 #REFER TO RENISHAW EN
+
+
+
+
+def electrical_system_init_setup(node, electrical_resistance: int=None, electrical_inductance: int=None):
+    #6.2.54
+    #electrical resistance of motor windings in mOhm
+    node.sdo[0x3002][0x1].raw = electrical_resistance if electrical_resistance is not None else 0
+    #electrical inductance of motor windings in uH
+    node.sdo[0x3002][0x2].raw = electrical_inductance if electrical_resistance is not None else 0
+
+
+
+
     
     
 def motor_setup(node, operating_mode: int):
