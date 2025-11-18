3f0cf88ef8dc937748c8a4ae3e6f348df7b160c0
lstuder-dev <laanze8@gmail.com>
2025-11-18 14:50:00 +0100

    Added func for encoder and motor init

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -32,21 +32,23 @@ def network_setup(node_id: int, node_eds: str, node_channel: str):
     return net, node
 
 
-def controller_init_setup():
-
-def motor_init_setup(nominal_current: int, current_lim: int, pole_pairs: int, th_t_const: int, tor_const: int, ):
+def motor_init_setup(node, nominal_current: int, current_lim: int, pole_pairs: int, therm_const: int, tor_const: int, ):
     #motor type (Sinusoidal PM BL motor = 10 or in hex 0x0A)
-    node.sdo[0x6402.0x00].raw = 0x000A
+    node.sdo[0x6402][0].raw = 0x000A
     #nominal current flowing through motor windings in mA
-    node.sdo[0x3001.0x01].raw = nominal_current
+    node.sdo[0x3001][1].raw = nominal_current
     #output current limit in mA
-    node.sdo[0x3001.0x02].raw = current_lim
+    node.sdo[0x3001][2].raw = current_lim
     #number of pole pairs
-    node.sdo[0x3001.0x03].raw = pole_pairs
+    node.sdo[0x3001][3].raw = pole_pairs
     #thermal time constant of windings
-    node.sdo[0x3001.0x04].raw = th_t_const
+    node.sdo[0x3001][4].raw = therm_const
     #torque constant of motor
-    node.sdo[0x3001.0x05].raw = tor_const
+    node.sdo[0x3001][5].raw = tor_const
+
+def encoder_init_setup(node, ):
+    print("blabla")
+
     
     
     
