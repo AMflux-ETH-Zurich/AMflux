cdf018058c8160ed971cbf0527f56ba501ef1b4d
lstuder-dev <laanze8@gmail.com>
2026-01-12 11:49:41 +0100

    checking encoder output

--- src/amflux/app/utils.py
+++ src/amflux/app/utils.py
@@ -70,6 +70,10 @@ def SSI_encoder_output_check(node, interval:float = 0.25, duration: int = 60):
     start_time = time.time()
     while (time.time()-start_time) < duration:
         pos_val = node.sdo[0x3012][0x0D].raw
+        b = node.sdo[0x3012][0x0D].raw
+
+        if abs(pos_val - b) > 1000:
+            print("Torn read detected", pos_val, b)
         print(f"Current SSI absolute position: {pos_val}")
         time.sleep(interval)
     
