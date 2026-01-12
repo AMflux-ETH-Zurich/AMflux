834f62f67ddf5cc5a660597a10e091702ac2f891
lstuder-dev <laanze8@gmail.com>
2026-01-12 16:42:54 +0100

    nun

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -78,7 +78,7 @@ def main():
     print(can_functions.cword_read(mc1))
     drive.goto_state(mc1, drive.DriveState.SWITCH_ON_DISABLED, timeout=5)
     print(can_functions.cword_read(mc1))
-
+    
     print(get_DriveState(mc1))
 
 
