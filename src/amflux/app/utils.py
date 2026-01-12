317125b8dd5f29c72d7e3c02e72da8c659c6466f
lstuder-dev <laanze8@gmail.com>
2026-01-12 14:30:06 +0100

    test

--- src/amflux/app/utils.py
+++ src/amflux/app/utils.py
@@ -79,7 +79,7 @@ def SSI_encoder_output_check(node, interval:float = 0.25, duration: int = 60):
     
     start_time = time.time()
     while (time.time()-start_time) < duration:
-        pos_val = node.tpdo[2][0x6064].raw
+        pos_val = node.tpdo[2]['pos_val'].raw
         
         print(f"Current SSI absolute position: {pos_val}")
         time.sleep(interval)
