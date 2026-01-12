bca859a3818aa25c6c6b0de0812f90ef1a9d99b1
lstuder-dev <laanze8@gmail.com>
2026-01-12 15:57:32 +0100

    test of cword

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -67,12 +67,10 @@ def main():
     
     can_functions.sanity_check(network)
 
-
+    object_dictionary_functions.mode_of_operation_init(mc1, 1)
 
     object_dictionary_functions.rxpdo_mapping_init(mc1)
     object_dictionary_functions.txpdo_mapping_init(mc1)
-
-    object_dictionary_functions.axis_configuration_init(mc1)
 
 
     print(can_functions.cword_read(mc1))
@@ -87,7 +85,7 @@ def main():
     ###print("Drive completed run. Network will be shutdown")
 
     #OUTPUT CHECK SSI ENCODER
-    utils.SSI_encoder_output_check(mc1, interval = 0.01, duration = 20)
+    #utils.SSI_encoder_output_check(mc1, interval = 0.01, duration = 20)
 
     can_functions.network_shutdown(net)
 
