12dcd30f7db460fd98f0dc2a203fc48290d5a497
lstuder-dev <laanze8@gmail.com>
2026-01-19 16:08:28 +0100

    typo

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -79,7 +79,7 @@ def main():
     print(f'var: {var}')
     drive.goto_state(mc1, desired_state=4, timeout=5)
     var2 = can_functions.sword(mc1)
-    print(f'var: {va2r}')
+    print(f'var: {var2}')
 
     '''
     can_functions.sanity_check(network)
