4fb82adc406504426759bfe14bee8cdd7b74e745
Wendecomprende <contactwendelin@gmail.com>
2026-02-18 14:14:27 +0100

    quick test for stop_volt()

--- src/amflux/app/organiser.py
+++ src/amflux/app/organiser.py
@@ -334,9 +334,10 @@ class DriveOrganiser:
         2. 
         3. 
         """
+        print("test a")
         if not self.power_enabled:
             return
-        
+        print("test b")
         goto_state(self.node, desired_state=DriveState.SWITCHED_ON, timeout=2)
         self.torque_enabled = False
         goto_state(self.node, desired_state=DriveState.READY_TO_SWITCH_ON, timeout=2)
