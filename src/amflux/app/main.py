b0dafdd871ef8b7c54a811296197b7aa7d7d9314
Wendecomprende <contactwendelin@gmail.com>
2026-02-25 18:31:15 +0100

    cursed

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -75,7 +75,7 @@ def main():
 
     can_functions.sanity_check(network)
 
-    print(mc1.object_dictionary.names["Axis configuration"])
+    print(mc1.object_dictionary.names["Axis configuration.1"])
 
     #object_dictionary_functions.pdo_mapping_init(mc1, network)
     EPOS4 = organiser.DriveOrganiser(mc1, network=network)
