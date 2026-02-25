14c95b9e042ebe72bb65acdf7a38285bc9328f60
Wendecomprende <contactwendelin@gmail.com>
2026-02-25 18:21:30 +0100

    sanita check for OD. prints to teerminal

--- src/amflux/app/main.py
+++ src/amflux/app/main.py
@@ -75,6 +75,8 @@ def main():
 
     can_functions.sanity_check(network)
 
+    print(mc1.object_dictionary)
+
     #object_dictionary_functions.pdo_mapping_init(mc1, network)
     EPOS4 = organiser.DriveOrganiser(mc1, network=network)
 
