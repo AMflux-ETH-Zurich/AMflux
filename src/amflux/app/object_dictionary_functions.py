8e399a11788042156d9cb8b1633b4a2d17ad56e5
Wendecomprende <contactwendelin@gmail.com>
2026-03-11 16:56:12 +0100

    change mode of operation to rpdo

--- src/amflux/app/object_dictionary_functions.py
+++ src/amflux/app/object_dictionary_functions.py
@@ -97,7 +97,6 @@ def pdo_mapping_init(node, network):
     node.tpdo[1].add_variable("Statusword") # Statusword
     node.tpdo[1].add_variable("Controlword")
     node.tpdo[1].add_variable("Modes of operation display")
-    node.tpdo[1].add_variable("Modes of operation")
     node.tpdo[1].trans_type = 255 #   1=SYNC; 255=asynchronous --> with every change of the 402 state machine (in 0x2400.04, bit mask 0x00000002 must be set (which is the default))
     node.tpdo[1].enabled = True
 
@@ -112,6 +111,7 @@ def pdo_mapping_init(node, network):
 
     # initialize Rpdo's    
     node.rpdo[1].add_variable("Controlword")  # Controlword
+    node.rpdo[1].add_variable("Modes of operation")
     node.rpdo[1].trans_type = 255 #   1=SYNC; 255=asynchronous
     node.rpdo[1].enabled = True
     
