c9501da9e4dddb5895615a9ebf99ea3c5d71d55b
Wendecomprende <contactwendelin@gmail.com>
2026-05-25 14:40:38 +0200

    Revert "Revert "removed torque reading""
    This reverts commit 735a79d7ec7f92b5ca2038c453d7431e76e638a8.

--- src/amflux/app/organiser.py
+++ src/amflux/app/organiser.py
@@ -444,9 +444,8 @@ class DriveOrganiser:
         """
         try:
             # DCF TPDO1: Statusword (0x6041)
-            # DCF TPDO3: Position actual value (0x6064)
+            # DCF TPDO3: Position actual value (0x6064), current actual value (0x30D1:02)
             # DCF TPDO4: Velocity actual value (0x606C)
-            # Current/torque is not mapped in the current DCF.
             # Non-blocking: read cached TPDO values (assume they've arrived during operation)
             
             status_word = self.node.tpdo[DCF_TPDO_STATUS]["Statusword"].raw
@@ -456,7 +455,9 @@ class DriveOrganiser:
             
             velocity = self.node.tpdo[DCF_TPDO_VELOCITY]["Velocity actual value"].phys
 
-            torque = self.node.sdo["Torque actual values"]["Torque actual value averaged"].raw
+            current = self.node.tpdo[DCF_TPDO_POSITION][
+                "Current actual values.Current actual value"
+            ].phys
                     
         except Exception as e:
             # Fallback to SDO reads (slower but safe)
@@ -471,9 +472,9 @@ class DriveOrganiser:
             except Exception:
                 velocity = None
             try:
-                torque = self.node.sdo[0x6077].raw
+                current = self.node.sdo["Current actual values"]["Current actual value"].phys
             except Exception:
-                torque = None
+                current = None
             try:
                 status_word = self.node.sdo[0x6041].raw
             except Exception:
@@ -483,7 +484,7 @@ class DriveOrganiser:
             #except Exception:
             #    current_mode_disp = None
         
-        self.recent_telemetry = [torque, velocity, position, status_word]
+        self.recent_telemetry = [current, velocity, position, status_word]
         return self.recent_telemetry
         
     def get_status(self) -> list:
