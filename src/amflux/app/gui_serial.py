5e71e75dfc6e98802a36f2c7a4510a91310ca570
Wendecomprende <contactwendelin@gmail.com>
2026-05-25 17:09:38 +0200

    print debug

--- src/amflux/app/gui_serial.py
+++ src/amflux/app/gui_serial.py
@@ -155,10 +155,12 @@ def build_serial_pid_panel(app, parent):
             samples = read_serial_pid_samples(app.serial_connection, serial_text_buffer)
             print(f"poll_serial: received {len(samples)} valid sample(s)")
 
-            if sample[0]==0 and sample[1]==0 and sample[2]==0:
+            if samples[0]==0 and samples[1]==0 and samples[2]==0:
+                
                 app.organiser.request_update_param("Position controller P gain", 3005138)
                 app.organiser.request_update_param("Position controller I gain", 606093)
                 app.organiser.request_update_param("Position controller D gain", 1109021)
+                print("using optimal values")
                 return
                 
            
